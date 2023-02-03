afe2d17b2492cea3894562c9c6316aecbfb7eae3
IVGlang <93157247+IVGlang@users.noreply.github.com>
2023-02-03 21:16:08 +0500

    Add files via upload

---
+++ adaptability/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var logo = document.getElementById("logo");
+    logo.onclick = function() {
+        var title = getComputedStyle(document.getElementById("title")).fontSize;
+        var p = getComputedStyle(document.getElementById("p")).fontSize;
+        alert("logo: " + getComputedStyle(logo).fontSize + "\ntitle: " + title + "\np:" + p);
+    }
+};
