127b453698076b65f95ab680b8815d7e57f85727
IVGlang <93157247+IVGlang@users.noreply.github.com>
2023-02-04 15:46:04 +0500

    Add files via upload

--- adaptability/index.js
+++ adaptability/index.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     var logo = document.getElementById("logo");
     var infoaboutcompany = document.getElementById("infoaboutcompany");
+    var version = document.getElementById("version");
+    var insidewindow = document.getElementById("insidewindow");
     logo.onclick = function() {
         var title = getComputedStyle(document.getElementById("title")).fontSize;
         var p = getComputedStyle(document.getElementById("p")).fontSize;
@@ -8,5 +10,13 @@ window.onload = function() {
     }
     infoaboutcompany.onclick = function() {
         alert("Незарегистрированный компания \"IVGlang\"");
+    }
+    version.onclick = function() {
+        document.getElementById("block").style.display = "block";
+        document.getElementById("window").style.display = "block";
+    }
+    insidewindow.onclick = function() {
+        document.getElementById("block").style.display = "none";
+        document.getElementById("window").style.display = "none";
     }
 };
